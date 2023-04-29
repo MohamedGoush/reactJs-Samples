1180ae15895731d1fc35f6fc12d3fc173ead37b9
MohamedGoush <0x1110111@gmail.com>
2023-04-29 15:44:32 +0530

    404 error page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/react_route/Home";
 import About from "./components/react_route/About";
+import OrderSummary from "./components/react_route/OrderSummary";
+import NoMatch from "./components/react_route/NoMatch";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/order-summary" element={<OrderSummary />}></Route>
+        <Route path="*" element={<NoMatch />}></Route>
       </Routes>
     </div>
   );
