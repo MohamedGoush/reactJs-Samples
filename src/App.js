539e49654a89a15236be23379852be5b3d45e79d
MohamedGoush <0x1110111@gmail.com>
2023-04-29 15:08:19 +0530

    react route added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
-import HookEffectOne from "./components/useEffect/HookEffectOne";
+// import HookEffectOne from "./components/useEffect/HookEffectOne";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/react_route/Home";
+import About from "./components/react_route/About";
 
 function App() {
   return (
     <div className="App">
-      <HookEffectOne />
+      {/* <HookEffectOne /> */}
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
